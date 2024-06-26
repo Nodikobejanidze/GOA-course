fd92d33f97d8eb4ef0db889947ce619900d57a1b
NodikoBejanidze <bejanidzenodiko@gmail.com>
2024-06-26 12:27:22 +0400

    second commit

---
+++ classwork/classwork.py
@@ -0,0 +1,25 @@
+Storing the flight number
+flight_n = "NO2311"
+
+Storing the flight information
+destination = "Korea"
+distance = 1650
+
+print(flight_n)
+print(destination)
+print(distance)
+
+
+name = "123Nodo"
+score = 80"
+
+print(e)
+print(s)
+
+
+salary = 150000
+pay_raise = 10000
+
+# Create a variable name new_salary
+
+print(new_salary)
