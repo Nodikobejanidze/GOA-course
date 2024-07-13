03a4791f75525c359e679c86ff98a14c072f6df6
NodikoBejanidze <bejanidzenodiko@gmail.com>
2024-07-13 16:35:26 +0400

    second commit

---
+++ level14/main.py
@@ -0,0 +1,11 @@
+number = int(input("Pleaseenter number (0-10):"))
+
+if number == 5:
+    print("You won!")
+else:
+    print("You Lost!")
+
+if number == 5+3 == 10 :
+    print("You won!")
+else:
+    print("You Lost!")
---
+++ level16/homework.py
@@ -0,0 +1,13 @@
+cars = []
+input("Please Enter Car Names")
+for i in range(8):
+    cars.append["Cars"]
+print(cars)
+
+
+
+
+
+
+
+
